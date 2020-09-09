f147f41cbcacfa2cbd4a0d82b28e2657e6dfdbeb
Casey B <caseyoksana@gmail.com>
2020-09-09 18:59:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,8 @@ def key_for_min_value(name_hash)
   min_key = nil 
 name_hash.each do |key, value|
   if value < min_value
-    min_value = key
+    min_value = value
+    min_key = key 
       binding.pry 
   end
   min_value
